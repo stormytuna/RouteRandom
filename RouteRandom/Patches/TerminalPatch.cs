f7c583f21f57d1b0d9844545fc88c5c72fdae661
stormytuna <stormytuna@outlook.com>
2023-11-25 00:39:42 +0000

    Add project files.

---
+++ RouteRandom/Patches/TerminalPatch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+
+namespace RouteRandom.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    public class TerminalPatch
+    {
+        private static TerminalKeyword routeKeyword;
+
+        private static readonly Random rand = new Random();
+
+        private static readonly TerminalKeyword randomKeyword = new TerminalKeyword {
+            word = "random",
+            name = "Random",
+            defaultVerb = routeKeyword
+        };
+
+        private static readonly TerminalNode routeRandomNode = new TerminalNode {
+            name = "routeRandom"
+        };
+
+        [HarmonyPostfix, HarmonyPatch("Awake")]
+        public static void AddNewTerminalWords(Terminal __instance) {
+            try {
+                routeKeyword = __instance.terminalNodes.allKeywords.First(kw => kw.name == "Route");
+
+                __instance.terminalNodes.allKeywords = __instance.terminalNodes.allKeywords.AddToArray(randomKeyword);
+                routeKeyword.compatibleNouns = routeKeyword.compatibleNouns.AddToArray(new CompatibleNoun {
+                    noun = randomKeyword,
+                    result = routeRandomNode
+                });
+            } catch {
+                RouteRandomBase.Log.LogError("Failed to add 'Random' keyword!");
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch("ParsePlayerSentence")]
+        public static TerminalNode RouteToRandomPlanet(TerminalNode __result, Terminal __instance) {
+            if (__result.name == routeRandomNode.name) {
+                // TODO: DRY this out
+                // TODO: Keyword to ignore all weather
+                // TODO: Config for any weather to ignore always
+                // TODO: Config to remove cost of costly planets
+                // TODO: Config to ignore costly planets
+
+                // Seems like all actual moons have buyRerouteToMoon set to -2
+                List<CompatibleNoun> routePlanetNodes = routeKeyword.compatibleNouns.Where(noun => noun.result.buyRerouteToMoon == -2 && noun.result.itemCost == 0).ToList();
+
+                int randomIndex = rand.Next(routePlanetNodes.Count);
+                return routePlanetNodes[randomIndex].result;
+            }
+
+            return __result;
+        }
+    }
+}
---
+++ RouteRandom/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace RouteRandom
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class RouteRandomBase : BaseUnityPlugin
+    {
+        public const string ModGUID = "stormytuna.RouteRandom";
+        public const string ModName = "Route Random";
+        public const string ModVersion = "1.0.0";
+
+        public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+        public static RouteRandomBase Instance;
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        void Awake() {
+            if (Instance is null) {
+                Instance = this;
+            }
+
+            Log.LogInfo("Route Random has awaken!");
+
+            harmony.PatchAll();
+        }
+    }
+}
