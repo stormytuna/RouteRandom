5420fe8ea5b75db38e43e963fca72bc8ad4327ee
stormytuna <stormytuna@outlook.com>
2024-02-27 20:35:00 +0000

    Remade project files and minor changes to code

--- RouteRandom/Helpers/RandomHelper.cs
+++ RouteRandom/Helpers/RandomHelper.cs
@@ -1,14 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace RouteRandom.Helpers
+namespace RouteRandom.Helpers;
+
+public static class RandomHelper
 {
-	public static class RandomHelper
-	{
-		public static TSource NextFromCollection<TSource>(this Random rand, IList<TSource> collection) {
-			int randomIndex = rand.Next(collection.Count());
-			return collection[randomIndex];
-		}
-	}
+    public static TSource NextFromCollection<TSource>(this Random rand, List<TSource> collection) {
+        int randomIndex = rand.Next(collection.Count);
+        return collection[randomIndex];
+    }
 }
